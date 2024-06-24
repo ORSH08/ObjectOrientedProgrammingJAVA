45bd1fade4bcf961b874c3d5fea75d5e89cae9b4
OrShemesh <fam.shemesh.or@gmail.com>
2024-06-24 16:19:42 +0300

    Second part of the project. 2.2 update of main

--- src/ObjectOriented10128/Main.java
+++ src/ObjectOriented10128/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//
 		TradeMarket tradeMarket = new TradeMarketSystem();
 
 		try (Scanner scanner = new Scanner(System.in)) {
